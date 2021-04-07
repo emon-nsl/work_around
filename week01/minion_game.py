654f651b7119c71a89902b90231bec8a76cfbf18
emon-nsl <81962047+emon-nsl@users.noreply.github.com>
2021-04-07 14:21:01 +0600

    Add files via upload

--- week01/minion_game.py
+++ week01/minion_game.py
@@ -1,1 +1,22 @@
-here in work around
+def minion_game(string):
+    vowel = ('A', 'E', 'I', 'O', 'U')
+    # print(string)
+    s = 0
+    k = 0
+    for i in range(len(string)):
+        if string[i] in vowel:
+            k += len(string) - i
+        else:
+            s += len(string) - i
+    
+    if k > s:
+        print('Kevin ' + str(k))
+        # print(s)
+    elif k ==s:
+        print('Draw')
+    else:
+        # print(k)
+        print('Stuart ' + str(s)) 
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
