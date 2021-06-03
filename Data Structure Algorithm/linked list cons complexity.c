3d072cb865d5f6b97d1ee2e42b62dcb0d562f1f5
emon-nsl <81962047+emon-nsl@users.noreply.github.com>
2021-06-03 10:02:02 +0600

    Update and rename linked list.c to linked list cons complexity.c

--- Data Structure Algorithm/linked list cons complexity.c
+++ Data Structure Algorithm/linked list cons complexity.c
@@ -32,29 +32,6 @@ void append(int roll)
 	}
 }
 
-// this apppend function takes O(n) time to append new node
-// void append(int roll)
-// {
-// 	if(root==NULL) //If the list is empty
-// 	{
-// 		root=new node();
-// 		root->roll=roll;
-// 		root->next=NULL;
-// 	}
-// 	else
-// 	{
-// 		node *current_node=root; //make a copy of root node
-// 		while(current_node->next!=NULL) //Find the last node
-// 		{
-// 			current_node=current_node->next; //go to next address
-// 		}
-// 		node *newnode = new node(); //create a new node
-// 		newnode->roll=roll;
-// 		newnode->next=NULL;
-		
-// 		current_node->next=newnode; //link the last node with new node
-// 	}
-// }
 
 void print()
 {
